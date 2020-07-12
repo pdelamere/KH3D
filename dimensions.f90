2d851ca9cdeb0d09b12616e9defba3c21c9c900b
Peter Delamere <padelamere@alaska.edu>
2020-07-12 14:23:53 -0800

    added fortran files

---
+++ dimensions.f90
@@ -0,0 +1,10 @@
+module dimensions
+      implicit none
+      save
+
+      integer, parameter:: nx = 159, ny = 179, nz = 121 !159,209,121
+
+      integer*4, parameter:: Ni_max = 50000000  
+      
+end module dimensions
+      
